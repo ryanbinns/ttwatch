6094bb8a2b1a9c6a8fab307d22d8827537c09af8
Dan Lenski <dlenski@gmail.com>
2016-05-07 21:23:22 -0700

    add error messages to EXPECT_* functions too (expands on e76b304aebcdfb692ad5794fd205da6451fd85e7)

--- ttops.h
+++ ttops.h
@@ -16,57 +16,6 @@
 #define H_TRANSFER 0x002b
 #define H_CHECK 0x002e
 
-static inline int
-EXPECT_BYTES(int fd, uint8_t *buf)
-{
-    uint16_t handle;
-    int length = att_read_not(fd, &handle, buf);
-    if (length < 0)
-        return length;
-    else if (handle != H_TRANSFER)
-        return -EBADMSG;
-    return (int)length;
-}
-
-static inline int
-EXPECT_LENGTH(int fd)
-{
-    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
-    uint16_t handle;
-    int length = att_read_not(fd, &handle, buf);
-    if (length < 0)
-        return length;
-    else if ((handle != H_LENGTH) || (length != 4))
-        return -EBADMSG;
-    return btohl(*((uint32_t*)buf));
-}
-
-static inline int
-EXPECT_uint32(int fd, uint16_t handle, uint32_t val)
-{
-    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
-    uint16_t h;
-    int length = att_read_not(fd, &h, buf);
-    if (length < 0)
-        return length;
-    else if ((h != handle) || (length != 4) || (btohl(*((uint32_t*)buf))!=val))
-        return -EBADMSG;
-    return 0;
-}
-
-static inline int
-EXPECT_uint8(int fd, uint16_t handle, uint8_t val)
-{
-    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
-    uint16_t h;
-    int length = att_read_not(fd, &h, buf);
-    if (length < 0)
-        return length;
-    else if ((h != handle) || (length != 1) || (*buf!=val))
-        return -EBADMSG;
-    return 0;
-}
-
 struct ble_dev_info {
     uint16_t handle;
     const char *name;
@@ -83,5 +32,68 @@ int tt_write_file(int fd, uint32_t fileno, int debug, const uint8_t *buf, uint32
 int tt_delete_file(int fd, uint32_t fileno);
 int tt_list_sub_files(int fd, uint32_t fileno, uint16_t **outlist);
 int tt_reboot(int fd);
+
+static inline int
+EXPECT_BYTES(int fd, uint8_t *buf)
+{
+    uint16_t handle;
+    int length = att_read_not(fd, &handle, buf);
+    if (length < 0)
+        return length;
+    else if (handle != H_TRANSFER) {
+        fprintf(stderr, "Expected 0x%04x <- BYTES, but got:\n   0x%04x <- ", H_TRANSFER, handle);
+        hexlify(stderr, buf, length, true);
+        return -EBADMSG;
+    }
+    return (int)length;
+}
+
+static inline int
+EXPECT_LENGTH(int fd)
+{
+    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
+    uint16_t handle;
+    int length = att_read_not(fd, &handle, buf);
+    if (length < 0)
+        return length;
+    else if ((handle != H_LENGTH) || (length != 4)) {
+        fprintf(stderr, "Expected 0x%04x <- (uint32_t)LENGTH, but got:\n  0x%04x <- ", H_LENGTH, handle);
+        hexlify(stderr, buf, length, true);
+        return -EBADMSG;
+    }
+    return btohl(*((uint32_t*)buf));
+}
+
+static inline int
+EXPECT_uint32(int fd, uint16_t handle, uint32_t val)
+{
+    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
+    uint16_t h;
+    int length = att_read_not(fd, &h, buf);
+    if (length < 0)
+        return length;
+    else if ((h != handle) || (length != 4) || (btohl(*((uint32_t*)buf))!=val)) {
+        fprintf(stderr, "Expected 0x%04x <- (uint32_t)0x%08x, but got:\n  0x%04x <- ", handle, val, h);
+        hexlify(stderr, buf, length, true);
+        return -EBADMSG;
+    }
+    return 0;
+}
+
+static inline int
+EXPECT_uint8(int fd, uint16_t handle, uint8_t val)
+{
+    uint8_t buf[BT_ATT_DEFAULT_LE_MTU];
+    uint16_t h;
+    int length = att_read_not(fd, &h, buf);
+    if (length < 0)
+        return length;
+    else if ((h != handle) || (length != 1) || (*buf!=val)) {
+        fprintf(stderr, "Expected 0x%04x <- (uint8_t)0x%02x, but got:\n  0x%02x <- ", handle, val, h);
+        hexlify(stderr, buf, length, true);
+        return -EBADMSG;
+    }
+    return 0;
+}
 
 #endif /* #ifndef __TTOPS_H__ */
