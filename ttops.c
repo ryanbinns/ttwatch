4471e8dce999673b5fe5a3485a0af3b225ee3bf9
Henrik Grimler <henrik@grimler.se>
2018-12-05 22:02:50 +0100

    tt_reboot: use overload code for v1 devices
    Will test v2 code on v1 device when I have the opportunity

--- ttops.c
+++ ttops.c
@@ -250,8 +250,18 @@ tt_authorize(TTDEV *d, uint32_t code, bool new_code)
 int
 tt_reboot(TTDEV *d)
 {
+    uint32_t bork = 0;
     uint8_t cmd[] = {0x07, 0, 0, 0};
-    att_wrreq(d->fd, d->h->cmd_status, cmd, sizeof cmd);
+    switch (d->protocol_version) {
+    case 1:
+        // ... then overwhelm the device with a torrent of zeros to the status register
+        for (int ii=1; ii<=1000; ii++) {
+            if (att_wrreq(d->fd, d->h->cmd_status, &bork, 4) < 0)
+                return ii;
+        }
+    case 2:
+        att_wrreq(d->fd, d->h->cmd_status, cmd, sizeof cmd);
+    }
     return -1;
 }
 
